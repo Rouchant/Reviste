0b9a70cba2d579adf7c3a8657d4257b9859d9a35
Rouchant <jmemarchant@gmail.com>
2026-04-19 03:54:14 -0400

    debug: add canary endpoint to Vercel API

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,8 @@
 import app from '../server/index';
+
+// Canary endpoint - if this works, the problem is deeper in the logic
+app.get('/api/canary', (req, res) => {
+  res.json({ status: 'alive', environment: process.env.NODE_ENV });
+});
+
 export default app;
